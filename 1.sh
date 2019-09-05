6589a93f4855e983aa6808c924e55c61c4df3939
zhaowenxin <3375887316@qq.com>
2019-09-05 13:18:18 +0800

    Update 1.sh
    修复bug

--- 1.sh
+++ 1.sh
@@ -18,14 +18,22 @@ REP_USER=rep;REP_PASSWORD=rep123456
 MYSQL_ROOT_PASSWORD=root123456
 
 
+
 #判断docker 管理程序是否安装
 if ! command -v docker > /dev/null;then
 	echo docker服务程序没有安装,现在开始安装....
 	apt update && apt install -y --no-install-recommends docker.io
+	systemctl enable docker
+	#将docker服务程序设置成系统自启动
 else
 	echo docker.io服务程序已安装.
 fi
-systemctl start docker
+
+#判断mysql-client-5.7程序是否安装
+if ! command -v mysql > /dev/null;then
+	echo mysql-client没有安装,现在开始安装....
+	apt install -y --no-install-recommends mysql-client-5.7
+fi
 
 #下载需要的docker image
 for image in "${REPOSITORY[@]}";do
@@ -41,6 +49,8 @@ for image in "${REPOSITORY[@]}";do
 		echo ${image}镜像已存在.
 	fi
 done
+
+ 
  
 #根据mgr_cnf配置文件,运行容器
 OLD_IFS="$IFS"
@@ -58,7 +68,7 @@ _get_name(){
 _get_volume(){
 local i=0
 local volume=($@)
-while [[ i -lt ${#volume[@]} ]]; do
+while [ ${i} -lt ${#volume[@]} ]; do
 	host_v[${i}]=/${name}$(echo ${volume[${i}]} | awk '{sub(/:(.)+/,"");print}')
 	container_v[${i}]=$(echo ${volume[${i}]} | awk '{sub(/(.)+:/,"");print}')
 	let i+=1
@@ -87,7 +97,7 @@ _cnf_set(){
 	group_mgr_cip=($(awk '$'${COLUMN_GROUP}' == "'"${arry_cloum[${COLUMN_GROUP}]}"'" \
 				   && $'${COLUMN_APP}' == "'"${APP_NAME_MGR}"'" \
 						{print $'${COLUMN_CONTAINER_IP}'}' mgr_cnf))
-	while [[ i -lt ${#group_mgr_cip[@]} ]]; do
+	while [ ${i} -lt ${#group_mgr_cip[@]} ]; do
 		if [ "${group_seeds}" != "" ] ;then
 			group_seeds=${group_seeds},
 		fi
@@ -104,10 +114,10 @@ _cnf_set(){
 }
 
 
-#初始化mysql_volume;mysql容器是依999:999的uid:gid来启动的
+#初始化mysql_volume;mysql容器是999:999的uid:gid来启动的
 _initial_mysql_volume(){
 	local i=0
-	while [[ i -lt ${#host_v[@]} ]]; do
+	while [ ${i} -lt ${#host_v[@]} ]; do
 		mkdir -p ${host_v[${i}]}
 		chown 999:999 ${host_v[${i}]}
 		let i+=1
@@ -118,7 +128,7 @@ _initial_mysql_volume(){
 # 创建容器;
 _create_mysql_container(){
 	i=0;dockerv=
-	while [[ i -lt ${#host_v[@]} ]];do
+	while [ ${i} -lt ${#host_v[@]} ];do
 		dockerv+=" -v ${host_v[${i}]}:${container_v[${i}]}"
 		let i+=1
 	done
@@ -146,14 +156,14 @@ _create_mysql_container(){
 }
 
 
-echo 本次运行脚本主要目的:
-echo 1 修改mysql配置文件
-echo 2 拷贝数据
-echo 3 引导集群启动
+echo '本次运行脚本主要目的:[1/2/3]?'
+echo 1 修改mysql配置文件;存在的容器修改,不存在的创建
+echo 2 复制数据;创建不存在的容器时,提示数据复制
+echo 3 引导集群启动;
 read main_cmd
 
 #检查mgr_cnf文件每行设置;
-line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
+line_i=1;while [ ${line_i} -lt ${#cnf_line[@]} ]; do
 #取到的行数据,cnf_line数组是从[0]开始,[0]是列表名行,不需要循环
 
 	#取到的每行的列数组arry_cloum也是从[0]开始,为了和awk一致,将[0]用字符占位
@@ -177,7 +187,7 @@ line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
 		#找到同一group组的主机IP,创建路由,保证同组,在不同主机的容器通讯
 		group_host=($(awk '$'${COLUMN_GROUP}' == "'"${arry_cloum[${COLUMN_GROUP}]}"'"{print $'${COLUMN_HOST_IP}'}' mgr_cnf))
 		j=0;k=0;df_host=()
-		while [[ j -lt ${#group_host[@]} ]];do
+		while [ ${j} -lt ${#group_host[@]} ];do
 			if [ ${group_host[${j}]} != ${arry_cloum[${COLUMN_HOST_IP}]} ];then
 			#找到同组IP
 				return_awk=$(echo "${df_host[@]}" | awk '/'${group_host[${j}]}'/ {print $0}')
@@ -192,7 +202,7 @@ line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
 
 		#判断路由条目是否存在;手工删除格式route del -net 192.168.21.0/27
 		j=0
-		while [[ j -lt ${#df_host[@]} ]];do
+		while [ ${j} -lt ${#df_host[@]} ];do
 			arry=$(awk '$'${COLUMN_GROUP}' == "'"${arry_cloum[${COLUMN_GROUP}]}"'" \
 				&& $'${COLUMN_HOST_IP}' == "'"${df_host[${j}]}"'" \
 				{print $'${COLUMN_GATEWAY}'}' mgr_cnf )
@@ -202,9 +212,22 @@ line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
 			return_awk=($(route | awk '$2 == "'"${df_host[${j}]}"'" {print $0}'))
 			if [ "${return_awk}" == "" ]; then
 			# 路由不存在,创建路由规则;需要修改/etc/sysctl.conf文件,否则创建了路由,也不能通讯
-				sed -i '/ip_forward/c ip_forward = 1' /etc/sysctl.conf
-				route add -net ${arry[0]}.${arry[1]}.${arry[2]}.$((${arry[3]}-1))/${MASK} \
-				gw ${df_host[${j}]}
+			# 重启后依然有效,需要修改/etc/sysconfig/static-routes
+				sed -i '/net\.ipv4\.ip_forward/c net.ipv4.ip_forward = 1' /etc/sysctl.conf
+				route_str="${arry[0]}.${arry[1]}.${arry[2]}.$((${arry[3]}-1))/${MASK} gw ${df_host[${j}]}"
+				route add -net ${route_str}
+				if [ ! -e /etc/sysconfig/static-routes ]; then
+					mkdir -p /etc/sysconfig
+					echo "any net ""${route_str}" > /etc/sysconfig/static-routes
+				else
+					return_awk=$(awk '/any net '${arry[0]}'\.'${arry[1]}'\.'${arry[2]}'\.'$((${arry[3]}-1))'/{print $0}' /etc/sysconfig/static-routes)
+					if [ "${return_awk}" == "" ]; then
+						echo "any net ""${route_str}" >> /etc/sysconfig/static-routes
+					else
+						sed -i '/any net '${arry[0]}'\.'${arry[1]}'\.'${arry[2]}'\.'$((${arry[3]}-1))'/c any net '"${route_str}"'' /etc/sysconfig/static-routes
+					fi
+				fi
+				
 				if [ "$?" == 0 ];then
 					echo ${arry_cloum[${COLUMN_GROUP}]}集群的\
 						${arry_cloum[${COLUMN_HOST_IP}]}到${df_host[${j}]}路由规则创建成功!
@@ -298,15 +321,19 @@ line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
 						esac
 					fi
 				fi
+				
+				#引导集群的mysql容器MEMBER_ROLE=primary;如果不是引导的本群组第一个mysql,需要手动重启跳过的mysql容器
 				if [ "${main_cmd}" == 3 ];then
-				#引导集群的第一个mysql的MEMBER_ROLE=primary;其他容器start group_replication;对单主的MGR,注意mgr.cnf的顺序
-					find=
+				#引导集群启动
+					already_bootstrap=
 					for i in "${already_bootstrap_group[@]}";do
 						if [ "${i}" == "${arry_cloum[${COLUMN_GROUP}]}" ];then
-							find=yes;break
+						#当前行的群组,已经进行过引导
+							already_bootstrap=yes;break
 						fi
 					done
-					if [ "${find}" != "yes" ] || [ "${already_bootstrap_group[0]}" == "" ];then
+					if [ "${already_bootstrap}" != "yes" ] ;then
+					#当前行的群组,没有进行过引导
 						echo 如果对已存在MEMBER_ROLE=primary的群组进行引导,将导致人为脑裂!
 						echo 请确认${arry_cloum[${COLUMN_GROUP}]}集群中,所有MGR成员的member_state都为offline,再进行引导!
 						echo 1 引导${arry_cloum[${COLUMN_GROUP}]}集群启动
@@ -317,11 +344,13 @@ line_i=1;while [[ line_i -lt ${#cnf_line[@]} ]]; do
 										start group_replication;\
 										set global group_replication_bootstrap_group=off;"
 								echo "${mgr_boot}" | "${mysql_str[@]}" &>/dev/null
+								already_bootstrap_group=(${already_bootstrap_group[@]} ${arry_cloum[${COLUMN_GROUP}]})
 								;;
-							2) 	;;
+							2) 	
+								;;
 						esac
-						already_bootstrap_group=(${already_bootstrap_group[@]} ${arry_cloum[${COLUMN_GROUP}]})
 					else
+					#当前行的群组,已经进行过引导;start group_replication当前mysql容器
 						echo 'start group_replication;' | "${mysql_str[@]}" &>/dev/null
 					fi
 				fi
